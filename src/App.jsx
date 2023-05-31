346a043877ffc068b889c9c317c8fb8ef2064f2b
Dhruv Pankaj Patel <dhruvkochi@gmail.com>
2023-06-01 03:20:37 +0530

    feat: navbar and aboutus basic layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 import React from "react"
 import Navbar from "./components/Navbar"
+import Aboutus from "./components/Aboutus"
 
 function App() {
 
 
   return (
     <>
-    
-      <Navbar />
+      <nav className="ml-28 mt-16">
+        <Navbar />
+      </nav>
+
+      <div className="float-right mr-[12%] mt-[4%]">
+        <Aboutus />
+      </div>
 
     </>
   )
