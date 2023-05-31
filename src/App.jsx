cce9806995458c63d28f35b79b594bef1c53dc88
Dhruv Pankaj Patel <dhruvkochi@gmail.com>
2023-06-01 00:52:33 +0530

    layout setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react"
+import Navbar from "./components/Navbar"
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-      Jurident Profile Page  
+    
+      <Navbar />
+
     </>
   )
+
 }
 
 export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+      This is the navbar 
+    </div>
+  )
+}
+
+export default Navbar
